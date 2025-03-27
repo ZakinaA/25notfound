81bfadb71e2ac6759983bdda2c1a963be8fb1b29
lucas marie sio <marielucas665@gmail.com>
2025-03-27 11:28:54 +0100

    implementation listerInterventions

---
+++ src/main/java/model/Situation.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author TS1SIO
+ */
+public class Situation {
+    
+    private int id;
+    private String type;
+
+    public Situation() {
+    }
+
+    public Situation(int id) {
+        this.id = id;
+    }
+
+    public Situation(int id, String type) {
+        this.id = id;
+        this.type = type;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+    
+    
+}
